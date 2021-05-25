4098e6b0f9c647ddd9e4dc2bfc498e698f75a9e7
berthakim <kte.ent@gmail.com>
2021-05-25 20:23:52 +0900

    Rewrite GET and add POST

--- arkeo/views.py
+++ arkeo/views.py
@@ -1,43 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Test
 from .serializers import TestSerializer
+import config
 
 
-class TestList(APIView):
-
-    def get(self, request):
-        test1 = Test.objects.all()
-        serializer = TestSerializer(test1, many=True)
+@api_view(['GET', 'POST'])
+def test_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Test.objects.all()
+        serializer = TestSerializer(snippets, many=True)
         return Response(serializer.data)
 
-
-    def post(self):
-        pass
-
-
-    def put(self):
-    	pass
-
-
-    def delete(self):
-    	pass
+    elif request.method == 'POST':
+        serializer = TestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    def path(self):
-    	pass
+@api_view(['GET', 'PUT', 'DELETE'])
+def test_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Test.objects.get(pk=pk)
+    except Test.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET':
+        serializer = TestSerializer(snippet)
+        return Response(serializer.data)
 
-    def head(self):
-    	pass
+    elif request.method == 'PUT':
+        serializer = TestSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-    def options(self):
-    	pass
-
-# def arkeo_api(request):
-#     return render(request, 'arkeo/arkeo_api.html', {'title': 'About'})
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
