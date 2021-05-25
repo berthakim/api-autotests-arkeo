8b64696783439669b8e6502db623f9fdef15677e
berthakim <kte.ent@gmail.com>
2021-05-25 20:12:49 +0900

    Updated url for main view (list) and Added url for details

--- arkeo/urls.py
+++ arkeo/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from . import views
+from .import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.TestList.as_view(), name='arkapi-map')
+    # path('', views.TestList.as_view(), name='arkapi-map')
+    path('', views.test_list),
+    path('snippets/<int:pk>/', views.test_detail),
+
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = format_suffix_patterns(urlpatterns)
