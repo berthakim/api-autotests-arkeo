2b64510acbb936d0ef721f4d931db063c1bdf851
tatianakim <tatianakime@gmail.com>
2021-11-30 02:43:14 -0900

    configured pass

---
+++ framework/clients/api_client.py
@@ -0,0 +1,34 @@
+import requests
+
+from framework.settings import URL, HEADERS
+
+
+class ApiClient:
+
+    def get_find_by_status(self, params, path="/v2/pet/findByStatus"):
+        """
+        :param params:
+        :param path:
+        :return:
+        """
+        response = requests.get(
+            url=f'{URL}{path}',
+            headers=HEADERS,
+            params=params
+        )
+        response.raise_for_status()
+        return response
+
+    def get_by_id(self, params, path="/v2/pet/{petId}"):
+        """
+        :param params:
+        :param path:
+        :return:
+        """
+        path = path.format(**params)
+        response = requests.get(
+            url=f'{URL}{path}',
+            headers=HEADERS,
+        )
+        response.raise_for_status()
+        return response
