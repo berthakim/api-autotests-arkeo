02aed85600b8e725f570c767f0c7e8867d9799a2
tatianakim <tatianakime@gmail.com>
2021-11-30 02:41:56 -0900

    edited fields of table meteostation

--- API_meteo/arkeo/serializers.py
+++ API_meteo/arkeo/serializers.py
@@ -6,15 +6,16 @@ from django.contrib.auth.models import User
 class MeteoStationSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    region = serializers.CharField(max_length=50)
+    st_type = serializers.ChoiceField(choices=STATION_TYPES, default="Not defined")
     lat = serializers.FloatField()
     lon = serializers.FloatField()
-    start = serializers.IntegerField()
-    st_type = serializers.ChoiceField(choices=STATION_TYPES, default="Not defined")
+    obs_beginning = serializers.IntegerField()
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = MeteoStation
-        fields = ['id', 'name', 'lat', 'lon', 'start', 'st_type']
+        fields = ['id', 'name', 'region', 'st_type', 'lat', 'lon', 'obs_beginning']
 
     def create(self, validated_data):
         """
@@ -27,10 +28,11 @@ class MeteoStationSerializer(serializers.Serializer):
         Update and return an existing `MeteoStation` instance, given the validated data.
         """
         instance.name = validated_data.get('name', instance.name)
+        instance.name = validated_data.get('region', instance.name)
+        instance.st_type = validated_data.get('st_type', instance.st_type)
         instance.lat = validated_data.get('lat', instance.lat)
         instance.lon = validated_data.get('lon', instance.lon)
-        instance.start = validated_data.get('start', instance.start)
-        instance.st_type = validated_data.get('st_type', instance.st_type)
+        instance.start = validated_data.get('obs_beginning', instance.start)
         instance.save()
         return instance
 
